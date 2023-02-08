9631201339e87d2ad2f89d38522339aaa3915f8a
Firstname Lastname <asupunjayasooriya@gmail.com>
2023-02-09 00:35:28 +0530

    view product

--- multivendor/resources/views/seller/viewproduct.blade.php
+++ multivendor/resources/views/seller/viewproduct.blade.php
@@ -75,9 +75,118 @@
             {{-- content start --}}
 
 
+           
+            <div class="col-lg-12 grid-margin stretch-card">
+              <div class="card">
+                <div class="card-body">
+                  <h4 class="card-title">Available Products</h4>
+                  <p class="card-description">
+                    List of <code>.Items</code>
+                  </p>
+                  <div class="table-responsive">
+                    <table class="table">
+                      <thead>
+                        <tr>
+                          <th>ID</th>
+                          <th>Title</th>
+                          <th>Qnt</th>
+                          <th>price</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        <tr>
+                          <td>Jacob</td>
+                          <td>53275531</td>
+                          <td>12 May 2017</td>
+                          <td>53275531</td>
+                          <td>
+                            <button type="button" class="btn btn-dark btn-icon-text">
+                                Edit
+                                <i class="ti-file btn-icon-append"></i>                          
+                              </button>
 
+                              <button type="button" class="btn btn-success btn-icon-text">
+                                <i class="ti-alert btn-icon-prepend"></i>                                                    
+                                Delete
+                              </button>
+                          </td>
+                          
+                        </tr>
+                        <tr>
+                          <td>Messsy</td>
+                          <td>53275532</td>
+                          <td>15 May 2017</td>
+                          <td>53275531</td>
+                          <td>
+                            <button type="button" class="btn btn-dark btn-icon-text">
+                                Edit
+                                <i class="ti-file btn-icon-append"></i>                          
+                              </button>
 
-           <h1>test</h1>
+                              <button type="button" class="btn btn-success btn-icon-text">
+                                <i class="ti-alert btn-icon-prepend"></i>                                                    
+                                Delete
+                              </button>
+                          </td>
+                        </tr>
+                        <tr>
+                          <td>John</td>
+                          <td>53275533</td>
+                          <td>14 May 2017</td>
+                          <td>53275531</td>
+                          <td>
+                            <button type="button" class="btn btn-dark btn-icon-text">
+                                Edit
+                                <i class="ti-file btn-icon-append"></i>                          
+                              </button>
+
+                              <button type="button" class="btn btn-success btn-icon-text">
+                                <i class="ti-alert btn-icon-prepend"></i>                                                    
+                                Delete
+                              </button>
+                          </td>
+                        </tr>
+                        <tr>
+                          <td>Peter</td>
+                          <td>53275534</td>
+                          <td>16 May 2017</td>
+                          <td>53275531</td>
+                          <td>
+                            <button type="button" class="btn btn-dark btn-icon-text">
+                                Edit
+                                <i class="ti-file btn-icon-append"></i>                          
+                              </button>
+
+                              <button type="button" class="btn btn-success btn-icon-text">
+                                <i class="ti-alert btn-icon-prepend"></i>                                                    
+                                Delete
+                              </button>
+                          </td>
+                        </tr>
+                        <tr>
+                          <td>Dave</td>
+                          <td>53275535</td>
+                          <td>20 May 2017</td>
+                          <td>53275531</td>
+                          <td>
+                            <button type="button" class="btn btn-dark btn-icon-text">
+                                Edit
+                                <i class="ti-file btn-icon-append"></i>                          
+                              </button>
+
+                              <button type="button" class="btn btn-success btn-icon-text">
+                                <i class="ti-alert btn-icon-prepend"></i>                                                    
+                                Delete
+                              </button>
+                          </td>
+                        </tr>
+                      </tbody>
+                    </table>
+                  </div>
+                </div>
+              </div>
+            </div>
+
 
             {{-- content end --}}
 
