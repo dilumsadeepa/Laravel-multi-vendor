73d4948c73a2dfa8c2047a8fc0660f1ffd0388ea
Firstname Lastname <asupunjayasooriya@gmail.com>
2023-02-08 16:35:04 +0530

    Update index.blade.php

--- multivendor/resources/views/seller/index.blade.php
+++ multivendor/resources/views/seller/index.blade.php
@@ -865,18 +865,18 @@
             <div class="col-md-12 grid-margin stretch-card">
               <div class="card">
                 <div class="card-body">
-                  <p class="card-title">Advanced Table</p>
+                  <p class="card-title">Orders</p>
                   <div class="row">
                     <div class="col-12">
                       <div class="table-responsive">
                         <table id="example" class="display expandable-table" style="width:100%">
                           <thead>
                             <tr>
-                              <th>Quote#</th>
+                              <th>Order No #</th>
                               <th>Product</th>
-                              <th>Business type</th>
-                              <th>Policy holder</th>
-                              <th>Premium</th>
+                              <th>Buyer Name</th>
+                              <th>Quentity</th>
+                              <th>Price</th>
                               <th>Status</th>
                               <th>Updated at</th>
                               <th></th>
