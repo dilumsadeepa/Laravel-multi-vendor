7e96951330fa3366d5d213701c578aaf7c5b3f23
LasithRandima <74246027+LasithRandima@users.noreply.github.com>
2023-02-09 01:08:21 +0530

    css linking

--- multivendor/resources/views/seller/addproduct.blade.php
+++ multivendor/resources/views/seller/addproduct.blade.php
@@ -1,78 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-  <!-- Required meta tags -->
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <title>Seller Dashbord</title>
-  <!-- plugins:css -->
-  <link rel="stylesheet" href="vendors/feather/feather.css">
-  <link rel="stylesheet" href="vendors/ti-icons/css/themify-icons.css">
-  <link rel="stylesheet" href="vendors/css/vendor.bundle.base.css">
-  <!-- endinject -->
-  <!-- Plugin css for this page -->
-  <link rel="stylesheet" href="vendors/datatables.net-bs4/dataTables.bootstrap4.css">
-  <link rel="stylesheet" href="vendors/ti-icons/css/themify-icons.css">
-  <link rel="stylesheet" type="text/css" href="js/select.dataTables.min.css">
-  <!-- End plugin css for this page -->
-  <!-- inject:css -->
-  <link rel="stylesheet" href="css/vertical-layout-light/style.css">
-  <!-- endinject -->
-  <link rel="shortcut icon" href="images/favicon.png" />
-</head>
-<body>
-  <div class="container-scroller">
-    <!-- partial:partials/_navbar.html -->
-    
-
-    {{-- nav start --}}
-
-    @include('seller.sellernav') 
-
-    {{-- nav end --}}
-
-
-    <!-- partial -->
-    <div class="container-fluid page-body-wrapper">
-      <!-- partial:partials/_settings-panel.html -->
-      <div class="theme-setting-wrapper">
-        <div id="settings-trigger"><i class="ti-settings"></i></div>
-        <div id="theme-settings" class="settings-panel">
-          <i class="settings-close ti-close"></i>
-          <p class="settings-heading">SIDEBAR SKINS</p>
-          <div class="sidebar-bg-options selected" id="sidebar-light-theme"><div class="img-ss rounded-circle bg-light border mr-3"></div>Light</div>
-          <div class="sidebar-bg-options" id="sidebar-dark-theme"><div class="img-ss rounded-circle bg-dark border mr-3"></div>Dark</div>
-          <p class="settings-heading mt-2">HEADER SKINS</p>
-          <div class="color-tiles mx-0 px-4">
-            <div class="tiles success"></div>
-            <div class="tiles warning"></div>
-            <div class="tiles danger"></div>
-            <div class="tiles info"></div>
-            <div class="tiles dark"></div>
-            <div class="tiles default"></div>
-          </div>
-        </div>
-      </div>
-
-      
-   
-
-
-
-      <!-- start side bar -->
-      
-      @include('seller.sellersidebar') 
-
-
-      {{-- end if sidebar --}}
-
-
-      <!-- partial -->
-      <div class="main-panel">
-        <div class="content-wrapper">
-
-            {{-- content start --}}
+<x-dashboard>
 
 
 
@@ -170,8 +96,8 @@
                             <div class="col-sm-9">
                                 <div class="form-group">
                                     <div class="form-group">
-                                        
-                                        
+
+
                                         <input type="file" name="img[]" class="file-upload-default">
                                         <div class="input-group col-xs-12">
                                           <input type="text" class="form-control file-upload-info" disabled placeholder="Upload Image">
@@ -193,7 +119,7 @@
                           </div>
                         </div>
                       </div>
-                     
+
                       <div class="row">
                         <div class="col-md-6">
                           <div class="form-group row">
@@ -210,7 +136,7 @@
                               <select class="form-control">
                                 <option>Used</option>
                                 <option>New</option>
-                                
+
                               </select>
                             </div>
                           </div>
@@ -219,59 +145,11 @@
 
                       <button type="submit" class="btn btn-primary mr-2">Submit</button>
                       <button class="btn btn-light">Cancel</button>
-                      
+
                     </form>
                   </div>
                 </div>
               </div>
 
 
-            {{-- content end --}}
-
-                
-              </div>
-            </div>
-        </div>
-        <!-- content-wrapper ends -->
-
-
-        <!-- footer start -->
-
-          @include('seller.sellerfooter') 
-          
-        <!-- footer end -->
-
-
-      </div>
-      <!-- main-panel ends -->
-    </div>   
-    <!-- page-body-wrapper ends -->
-  </div>
-  <!-- container-scroller -->
-
-  <!-- plugins:js -->
-  <script src="vendors/js/vendor.bundle.base.js"></script>
-  <!-- endinject -->
-  <!-- Plugin js for this page -->
-  <script src="vendors/chart.js/Chart.min.js"></script>
-  <script src="vendors/datatables.net/jquery.dataTables.js"></script>
-  <script src="vendors/datatables.net-bs4/dataTables.bootstrap4.js"></script>
-  <script src="js/dataTables.select.min.js"></script>
-
-  <!-- End plugin js for this page -->
-  <!-- inject:js -->
-  <script src="js/off-canvas.js"></script>
-  <script src="js/hoverable-collapse.js"></script>
-  <script src="js/template.js"></script>
-  <script src="js/settings.js"></script>
-  <script src="js/todolist.js"></script>
-  <!-- endinject -->
-  <!-- Custom js for this page-->
-  <script src="js/dashboard.js"></script>
-  <script src="js/Chart.roundedBarCharts.js"></script>
-  <!-- End custom js for this page-->
-</body>
-
-</html>
-
-
+</x-dashboard>
