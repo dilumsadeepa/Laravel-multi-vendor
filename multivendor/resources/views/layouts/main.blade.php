bfaf163998033881fb002cbaea513f4eeb24e548
Dilum Sadeepa <51749100+dilumsadeepa@users.noreply.github.com>
2023-02-08 23:40:10 +0530

    register

---
+++ multivendor/resources/views/layouts/main.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Multi Vendor</title>
+    <!-- Latest compiled and minified CSS -->
+    <link href="{{ asset('main/bootstrap/bootstrap.min.css') }}" rel="stylesheet">
+
+    <!-- Latest compiled JavaScript -->
+    <script src="{{ asset('main/bootstrap/bootstrap.bundle.min.js') }}"></script>
+</head>
+<body>
+
+    @yield('content')
+
+</body>
+</html>
