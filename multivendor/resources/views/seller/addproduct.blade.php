847e68e27fe9bd82689802997b067a9ae54372ef
Dilum Sadeepa <51749100+dilumsadeepa@users.noreply.github.com>
2023-02-12 12:16:21 +0530

    product updated

--- multivendor/resources/views/seller/addproduct.blade.php
+++ multivendor/resources/views/seller/addproduct.blade.php
@@ -72,6 +72,18 @@
                             </div>
                           </div>
                         </div>
+
+                        <div class="col-md-6">
+                            <div class="form-group row">
+                              <label class="col-sm-3 col-form-label">Sub Category</label>
+                              <div class="col-sm-9">
+                                <input type="text" class="form-control" placeholder="" name="psubcat"/>
+                              </div>
+                            </div>
+                          </div>
+
+                      </div>
+                      <div class="row">
                         <div class="col-md-6">
                           <div class="form-group row">
                             <label class="col-sm-3 col-form-label">Product</label>
