f0e60d0fed3dec6db267fce9da9a423b7e588741
WarunaDisanayaka <86902580+WarunaDisanayaka@users.noreply.github.com>
2023-02-12 13:01:48 +0530

    Updated migrations

--- multivendor/database/migrations/2023_02_08_181904_create_products_table.php
+++ multivendor/database/migrations/2023_02_08_181904_create_products_table.php
@@ -18,8 +18,8 @@ return new class extends Migration
             $table->string('pname');
             $table->string('pimg');
             $table->string('pprice');
-            $table->string('pdis');
-            $table->string('pshort');
+            $table->text('pdis');
+            $table->text('pshort');
             $table->string('pshopid');
             $table->string('psubcat');
             $table->string('pcatid');
