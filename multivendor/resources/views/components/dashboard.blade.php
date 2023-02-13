4e33e6351d303b9709389e182bbc6b43b9d843b2
Firstname Lastname <asupunjayasooriya@gmail.com>
2023-02-13 17:07:02 +0530

    fix

--- multivendor/resources/views/components/dashboard.blade.php
+++ multivendor/resources/views/components/dashboard.blade.php
@@ -128,10 +128,10 @@ $time = Carbon::now()->format('H:i:s');
               </div>
             </li> --}}
             <li class="nav-item nav-profile dropdown">
-              <a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown" id="profileDropdown">
+              <a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown" id="profileDropdown" style="border:none;">
                 {{-- @if (Auth::user()->profile_photo_url) --}}
                         @if (Laravel\Jetstream\Jetstream::managesProfilePhotos())
-                        <button class="flex text-sm border-2 border-transparent rounded-full focus:outline-none focus:border-gray-300 transition">
+                        <button class="flex text-sm border-0 border-transparent rounded-full focus:outline-none transition">
                             <img class="h-8 w-8 rounded-full object-cover" src="{{ Auth::user()->profile_photo_url }}" alt="{{ Auth::user()->firstname }}" />
                         </button>
                     @else
